25f2a849338b47fda8336e5ba47fe0678cc7697c
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2026-04-04 18:54:13 +0700

    Update Experience.jsx to enhance work experience section; add details for Viettel Software and TLH VietNam

--- src/components/PageFragments/HomePage/Experience.jsx
+++ src/components/PageFragments/HomePage/Experience.jsx
@@ -25,21 +25,39 @@ const Experience = () => (
       <p style={{ marginBottom: '1em' }}>
         <Row>
           <Col xs={7}>
-            <h3 style={{ marginBottom: '0.3em' }}>VTI (VTI Group)</h3>
+            <h3 style={{ marginBottom: '0.3em' }}>Viettel Software (VTIT)</h3>
           </Col>
         </Row>
-        <li style={{ marginLeft: '1rem' }}>Develop the Mobi system (a ride-hailing system for 3 markets: Japan, Malaysia, and Vietnam)</li>
-        <li style={{ marginLeft: '1rem' }}>Developing a facial recognition payment system for buses in Japan.</li>
+        <li style={{ marginLeft: '1rem' }}>Project development by the corporation and the State Securities Commission.</li>
+        <li style={{ marginLeft: '1rem' }}>Developing projects for Viettel Group</li>
       </p>
 
       <p style={{ marginBottom: '1em' }}>
         <Row>
           <Col xs={7}>
-            <h3 style={{ marginBottom: '0.3em' }}>Viettel Telecom</h3>
+            <h3 style={{ marginBottom: '0.3em' }}>VTI (VTI Group)</h3>
           </Col>
         </Row>
-        <li style={{ marginLeft: '1rem' }}>Developing a Java backend microservice system (SME-HUB)</li>
-        <li style={{ marginLeft: '1rem' }}>Java (SpringBoot Framework), MVC pattern, BPMN flow, SOAP API Ehcache, Jenkin (CI/CD), K8S, Docker</li>
+        <li style={{ marginLeft: '1rem' }}>Develop the Mobi system (a ride-hailing system for 3
+          markets: Japan, Malaysia, and Vietnam)
+        </li>
+        <li style={{ marginLeft: '1rem' }}>Developing a facial recognition payment system for buses
+          in Japan.
+        </li>
+      </p>
+
+      <p style={{ marginBottom: '1em' }}>
+        <Row>
+          <Col xs={7}>
+            <h3 style={{ marginBottom: '0.3em' }}>TLH VietNam</h3>
+          </Col>
+        </Row>
+        <li style={{ marginLeft: '1rem' }}>Onsite Viettel Telecom: Developing a Java backend
+          microservice system (SME-HUB)
+        </li>
+        <li style={{ marginLeft: '1rem' }}>Java (SpringBoot Framework), MVC pattern, BPMN flow, SOAP
+          API Ehcache, Jenkin (CI/CD), K8S, Docker
+        </li>
       </p>
 
       <p style={{ marginBottom: '1em' }}>
@@ -48,8 +66,12 @@ const Experience = () => (
             <h3 style={{ marginBottom: '0.3em' }}>Cen Land (Cen Group)</h3>
           </Col>
         </Row>
-        <li style={{ marginLeft: '1rem' }}>Actively contributed to developing an Android application (Fastee)</li>
-        <li style={{ marginLeft: '1rem' }}>Focused on golf course booking alongside building foundational backend components to support the app’s functionality.</li>
+        <li style={{ marginLeft: '1rem' }}>Actively contributed to developing an Android application
+          (Fastee)
+        </li>
+        <li style={{ marginLeft: '1rem' }}>Focused on golf course booking alongside building
+          foundational backend components to support the app’s functionality.
+        </li>
       </p>
     </div>
   </div>
